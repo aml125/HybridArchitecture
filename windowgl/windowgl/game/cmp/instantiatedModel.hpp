4a3b0a5f56fadf2c7fad7817a4d3de51b6636620
Antonio Maciá Lillo <aml125@alu.ua.es>
2020-12-22 12:25:33 +0100

    created new model component

---
+++ windowgl/windowgl/game/cmp/instantiatedModel.hpp
@@ -0,0 +1,12 @@
+#include <game/cmp/model.hpp>
+
+namespace GM {
+	struct InstantiatedModel_t : Model_t
+	{
+		/*  Functions   */
+		InstantiatedModel_t(std::size_t eid)
+			: Model_t{ eid }
+		{
+		};
+	};
+}
--- windowgl/windowgl/game/util/entitybuilder.hpp
+++ windowgl/windowgl/game/util/entitybuilder.hpp
@@ -15,7 +15,9 @@ namespace GM {
 		//static ECS::Entity_t& buildGround(ECS::EntityManager_t& em);
 		
 		static ECS::Entity_t& buildEntityWithModelAndCollision(GM::GameManager& em, const std::string& modelPath, const glm::vec3& colliderLength, const glm::vec3& colliderOffset);
+		static ECS::Entity_t& buildInstantiatedEntityWithModelAndCollision(GameManager& gm, const std::string& modelPath, const glm::vec3& colliderLength, const glm::vec3& colliderOffset);
 		static ECS::Entity_t& buildFullEntity(GM::GameManager& em, const glm::vec3& position, const std::string& modelPath, const glm::vec3& colliderLength, const glm::vec3& colliderOffset);
+		static ECS::Entity_t& buildFullInstantiatedEntity(GameManager& gm, const glm::vec3& position, const std::string& modelPath, const glm::vec3& colliderLength, const glm::vec3& colliderOffset);
 		static ECS::Entity_t& buildNPC(GameManager& gm, const glm::vec3& position, const std::string& modelPath, unsigned int patternNumber, IASystem_t& iaSystem);
 		static GM::Pattern& buildPattern(GameManager& gm, IASystem_t& iaSys, unsigned int totalSlots, unsigned int depth, float separation, const std::string& modelPath, const glm::vec3& initialPosition);
 	};
